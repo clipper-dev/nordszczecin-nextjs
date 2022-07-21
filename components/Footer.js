25a4f748a17efde4368def028595cb8fb70f5eed
clipper-dev <96745726+clipper-dev@users.noreply.github.com>
2022-07-22 00:45:41 +0200

    Prototype

---
+++ components/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './Footer.module.css'
+import Link from 'next/link'
+
+//icons
+import { FaMediumM, FaLinkedinIn, FaLink, FaInstagram, FaTwitter, FaBook, FaReadme } from 'react-icons/fa'
+
+function Footer() {
+    
+    return (
+        <div className={styles['main-footer-container']}>
+            <div className={styles['footer-container']}>
+
+                <div className={styles['footer-column']}>
+                    <div className={styles['footer-header']}>NORD Szczecin</div>
+                    <span className={styles['footer-content']}>NORD Szczecin Sp. z o.o.</span>
+                    <span className={styles['footer-content']}>ul. Gdańska 3c </span>
+                    <span className={styles['footer-content']}>71-660 Szczecin</span>
+                    <span className={styles['footer-content']}>NIP 955-25-40-182</span>
+                </div>
+
+                <div className={styles['footer-column']}>
+
+                    <div className={styles['footer-header']}>KONTAKT</div>
+                    <a href="00-48-888-333-891" className={styles['footer-content']}>+48 888 333 891</a>
+                    <span className={styles['footer-content']}>biuro@nordszczecin.pl</span>
+                    
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Navbar.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import { Button } from './Button'
+import styles from './Navbar.module.css'
+import { NavBarData } from './NavBarData'
+import { SideBarData } from './SideBarData'
+import Link from 'next/link'
+import { FaBars } from 'react-icons/fa'
+import { useWindowSize } from '../utils/CustomHooks'
+
+function Navbar() {
+
+	const [sideBar, setSideBar] = useState(false);
+	const showSideBar = () => setSideBar(!sideBar);
+	const [button] = useState(true);
+	{/*Custom hook responsible for detecting small screen like mobiles*/ }
+	const size = useWindowSize();
+
+	return (
+		<>
+			<div className={styles['navbar']}>
+				<div className={styles['title-container']}>
+					{/*Left section of the navbar, responsible for the logo or company name*/}
+					<Link href={'/'} onClick={() => { setSideBar(false) }} passHref >
+						{/*Changes depending on the width of the screen*/}
+						{size.width > 600 ? <span className={styles['app-title']}>Nord Szczecin - Kuchnie na wymiar</span> : <span className={styles['app-title']}>Nord</span>}
+					</Link>
+				</div>
+				{size.width > 600 ?
+
+					<div className={styles['navBar-menu']}>
+						<ul className={styles['navBar-menu-items']}>
+							{NavBarData.map((item, index) => {
+								return (
+									<li key={index} className={styles['nav-text-top']}>
+										<Link href={item.path} onClick={() => { setSideBar(false) }} passHref >
+											<span className={[styles['navBar-menu-item-title'], ['p--display']].join(" ")}>{item.title}</span>
+										</Link>
+									</li>
+								)
+							})}
+						</ul>
+
+					</div>
+					:
+					<div className={styles['menu-icon']}>
+						<FaBars onClick={() => { setSideBar(!sideBar) }} />
+					</div>
+				}
+			</div>
+			{/*Display side bar only if opened and only if on mobile*/}
+			{<div className={sideBar ? [styles['sideBar-menu'], styles['sideBar-active']].join(" ") : [styles['sideBar-menu'], styles['sideBar-hidden']].join(" ")}>
+				<ul className={styles['sideBar-menu-items']}>
+					{SideBarData.map((item, index) => {
+						return (
+							<li key={index} className={styles['sideBar-menu-item']}>
+								<Link href={item.path} onClick={() => { setSideBar(false) }} passHref >
+									<span className={[styles['sideBar-menu-item-text'], ['p--display']].join(" ")}>{item.title}</span>
+								</Link>
+							</li>
+						)
+					})}
+				</ul>
+			</div>}
+		</>
+	)
+}
+
+function NavbarSinglePage() {
+
+	const [sideBar, setSideBar] = useState(false);
+	const showSideBar = () => setSideBar(!sideBar);
+	const [button] = useState(true);
+	{/*Custom hook responsible for detecting small screen like mobiles*/ }
+	const size = useWindowSize();
+
+	return (
+		<>
+			<div className={styles['navbar']}>
+				<div className={styles['title-container']}>
+					{/*Left section of the navbar, responsible for the logo or company name*/}
+					<Link href={'/'} onClick={() => { setSideBar(false) }} passHref >
+						{/*Changes depending on the width of the screen*/}
+						{size.width > 600 ? <span className={styles['app-title']}>Nord Szczecin - Kuchnie na wymiar</span> : <span className={styles['app-title']}>Nord</span>}
+					</Link>
+				</div>
+				{size.width > 600 ?
+
+					<div className={styles['navBar-menu']}>
+						<ul className={styles['navBar-menu-items']}>
+							{NavBarData.map((item, index) => {
+								return (
+									<li key={index} className={styles['nav-text-top']}>
+										<a href={`#${item.path}`} onClick={(e) => {
+											e.preventDefault();
+											document.querySelector(`#${item.path}`).scrollIntoView({
+												behavior: "smooth"
+											});
+										}} >
+											<span className={[styles['navBar-menu-item-title'], ['p--display']].join(" ")}>{item.title}</span>
+										</a>
+									</li>
+								)
+							})}
+						</ul>
+
+					</div>
+					:
+					<div className={styles['menu-icon']}>
+						<FaBars onClick={() => { setSideBar(!sideBar) }} />
+					</div>
+				}
+			</div>
+			{/*Display side bar only if opened and only if on mobile*/}
+			{<div className={sideBar ? [styles['sideBar-menu'], styles['sideBar-active']].join(" ") : [styles['sideBar-menu'], styles['sideBar-hidden']].join(" ")}>
+				<ul className={styles['sideBar-menu-items']}>
+					{SideBarData.map((item, index) => {
+						return (
+							<li key={index} className={styles['sideBar-menu-item']}>
+								<Link href={item.path} onClick={() => { setSideBar(false) }} passHref >
+									<span className={[styles['sideBar-menu-item-text'], ['p--display']].join(" ")}>{item.title}</span>
+								</Link>
+							</li>
+						)
+					})}
+				</ul>
+			</div>}
+		</>
+	)
+}
+
+
+export { Navbar, NavbarSinglePage }
